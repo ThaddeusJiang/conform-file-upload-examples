834ed53e219d4aca5605d327eee289d7a6cf0af8
TJ <thaddeusjiang@gmail.com>
2024-04-25 16:49:02 +0900

    zod schema for input["type=file"]

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,40 @@
-import type { MetaFunction } from "@remix-run/node";
+import { useForm } from "@conform-to/react"
+import { parseWithZod } from "@conform-to/zod"
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { Form } from "@remix-run/react"
+import { z } from "zod"
 
-export default function Index() {
+const schema = z.object({
+  files: z
+    .unknown() // I know the instance, should be File or File[], not FileList.
+    .refine((value) => {
+      const files = Array.isArray(value) ? value : [value]
+      return files.length > 0
+    }, "At least 1 file is required")
+    .refine((value) => {
+      const files = Array.isArray(value) ? value : [value]
+      return files.every((file) => file.size < 1024 * 1024 * 10)
+    }, "File size must be less than 10MB"),
+})
+
+export default function Example() {
+  const [form, fields] = useForm({
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema })
+    },
+    shouldValidate: "onInput",
+  })
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <Form method="POST" encType="multipart/form-data" id={form.id} onSubmit={form.onSubmit}>
+      <div>
+        <label htmlFor={fields.files.id}>Multiple Files: </label>
+        <br />
+        <input type="file" name={fields.files.name} multiple />
+        <div>{Object.entries(fields.files.allErrors).flatMap(([, messages]) => messages)}</div>
+        {/* {fields.files.errors && <div>{fields.files.errors}</div>} */}
+      </div>
+      <button>Upload</button>
+    </Form>
+  )
 }
